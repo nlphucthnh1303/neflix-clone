31c20701717ea7b397552c36f940c5e5031018b3
PhucThinh <nlphucthnh@gmail.com>
2025-11-23 20:11:17 +0700

    update code

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,8 +1,9 @@
 import {IconDefinition} from "@fortawesome/free-brands-svg-icons"
-import {faCircleInfo, faPlay, faSearch} from "@fortawesome/free-solid-svg-icons"
+import {faBell, faCircleInfo, faPlay, faSearch} from "@fortawesome/free-solid-svg-icons"
 
 export const fontAwesomeIcons: IconDefinition[] = [
     faPlay,
     faCircleInfo,
-    faSearch
+    faSearch,
+    faBell
 ]
